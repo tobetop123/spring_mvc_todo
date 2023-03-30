1e22051cbd33cb979654c2edc5ed9f2f474c6f26
이동하 <tobetop2@naver.com>
2023-03-30 23:02:22 +0900

    Test 연습

---
+++ src/test/java/com/multicampus/springboot/controller/MyControllerTest.java
@@ -0,0 +1,40 @@
+package com.multicampus.springboot.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyControllerTest {
+
+    @Test
+    void root() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void registerProcess() {
+    }
+
+    @Test
+    void view() {
+    }
+
+    @Test
+    void modify() {
+    }
+
+    @Test
+    void modifyProcess() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void list() {
+    }
+}
